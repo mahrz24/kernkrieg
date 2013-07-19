b76212dab65ec678f69e951c5866e3b36e798dc4
Malte Harder <me@malteharder.de>
2013-07-19 15:30:24 +0200

    Added password row for table

--- angular/app/scripts/controllers/accounts.js
+++ angular/app/scripts/controllers/accounts.js
@@ -4,18 +4,33 @@ angular.module('kkApp')
 .controller('AccountsCtrl',
   ['$scope', 'accounts', 'User',
   function ($scope, accounts, User) {
+    var passwordCell = '<div class="ngCellText" ng-class="col.colIndex()">' +
+      '<span ng-cell-text>******</span></div>';
+
+    var editCell = '<input type="text" ng-cell-input ng-class="\'colt\' + $index" ng-input="COL_FIELD" ng-model="COL_FIELD"></input>';
+    var passwordEditCell = '<input type="password" ng-cell-input ng-class="\'colt\' + $index" ng-input="COL_FIELD" ng-model="COL_FIELD"></input>';
+
     $scope.accounts = accounts;
     $scope.gridOptions =
     {
       data: 'accounts',
       enableCellSelection: true,
       enableRowSelection: false,
+      rowHeight:50,
       columnDefs: [ { field: 'username',
                       displayName: 'Username',
-                      enableCellEdit: true
+                      enableCellEdit: true,
+                      editableCellTemplate: editCell,
                     },
                     { field:'email',
                       displayName:'E-Mail',
+                      enableCellEdit: true,
+                      editableCellTemplate: editCell,
+                    },
+                    { field:'passwd_hash',
+                      displayName:'Password',
+                      cellTemplate: passwordCell,
+                      editableCellTemplate: passwordEditCell,
                       enableCellEdit: true
                     } ]
     };
@@ -31,6 +46,7 @@ angular.module('kkApp')
           if(!angular.equals(oldValue[i],newValue[i]) && oldValue[i].id == newValue[i].id)
           {
             // Update account
+            console.log("Updating " + i);
             var user = new User($scope.accounts[i]);
             user.$update(function(user) {
             }, function() {
