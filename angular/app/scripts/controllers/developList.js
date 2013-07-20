a37bbbb7147cd60fd9523c8eb0e105bd5958a453
Malte Harder <me@malteharder.de>
2013-07-20 17:27:50 +0200

    Warrior creation works

--- angular/app/scripts/controllers/developList.js
+++ angular/app/scripts/controllers/developList.js
@@ -2,8 +2,22 @@
 
 angular.module('kkApp')
 .controller('DevelopListCtrl',
-  ['$scope', 'own_warriors',
-  function ($scope, own_warriors)
+  ['$scope', '$http', 'own_warriors', 'Warrior',
+  function ($scope, $http, own_warriors, Warrior)
   {
+    $http.get('/api/user_id').success(function(data, status, headers, config) {
+        $scope.user_id =  data.user_id;
+    });
+
     $scope.own_warriors = own_warriors;
+
+    $scope.addNewWarrior = function ()
+    {
+      var warrior = new Warrior({ name: "Untitled warrior", code: "",
+        public: false, testable: false, owners : [{id: $scope.user_id}]});
+
+      warrior.$save( function(warrior) {
+        $scope.own_warriors.unshift(warrior);
+      });
+    };
   }]);
