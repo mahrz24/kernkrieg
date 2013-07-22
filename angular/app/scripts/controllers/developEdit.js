55a16c09122a7325acf646fb00a20e4b6aac7c52
Malte Harder <me@malteharder.de>
2013-07-22 18:28:25 +0200

    Selection of test warrior possible

--- angular/app/scripts/controllers/developEdit.js
+++ angular/app/scripts/controllers/developEdit.js
@@ -1,13 +1,21 @@
 angular.module('kkApp')
 .controller('DevelopEditCtrl',
-  ['$scope', '$http', '$location', 'warrior', 'Warrior',
-  function ($scope, $http, $location, warrior, Warrior)
+  ['$scope', '$http', '$location', 'warrior', 'Warrior', 'testables',
+  function ($scope, $http, $location, warrior, Warrior, testables)
   {
     $scope.warrior = warrior;
+    $scope.testables = testables.results;
+    $scope.testSelection = $scope.testables[0];
 
     $scope.saveWarrior = function ()
     {
       $scope.warrior.$update();
+    }
+
+    $scope.changeTestSelection = function(testable)
+    {
+      console.log("Hello");
+      $scope.testSelection = testable;
     }
 
   }]);
